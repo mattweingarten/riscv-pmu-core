59db73732f0b111464566d9984cad331708d197f
mattweingarten <matthew.weingarten@inf.ethz.ch>
2024-12-13 23:04:07 -0500

    Fix setup

--- setup.sh
+++ setup.sh
@@ -2,16 +2,15 @@
 
 
 git submodule update --init
-cd chipyard/
 
+cd chipyard/
 git remote set-url origin git@github.com:mattweingarten/chipyard.git
 git checkout rocket_chip_cpi
+./build-setup.sh riscv-tools  -s 4 -s 5 -s 6 -s 7 -s 8 -s 9 
 
 
 cd generators/rocket-chip/
 git remote set-url origin git@github.com:mattweingarten/rocket-chip.git
 git checkout rocket_chip
-cd ../../
+cd ../../../
 
-./build-setup.sh riscv-tools  -s 4 -s 5 -s 6 -s 7 -s 8 -s 9 
-cd ../
