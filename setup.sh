082dc7afaf37008321afeee6abb56b445e76f6d1
mattweingarten <matthew.weingarten@inf.ethz.ch>
2024-12-13 22:33:33 -0500

    Fix setup

--- setup.sh
+++ setup.sh
@@ -17,17 +17,17 @@ cd ../../
 
 cd generators/boom/
 git remote set-url origin git@github.com:mattweingarten/riscv-boom.git
-git checkout boom_chip
 cd ../../
 
 
 
-./build-setup.sh riscv-tools  -s 4 -s 5 -s 6 -s 7 -s 8 -s 9 
-cd ../
 
-cd riscv-test
+cd riscv-tests
 
 
 git remote set-url origin git@github.com:mattweingarten/riscv-tests.git
-cd .. /
+cd ../
 
+cd chipyard/
+./build-setup.sh riscv-tools  -s 4 -s 5 -s 6 -s 7 -s 8 -s 9 
+cd ../
